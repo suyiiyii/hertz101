a21b42168aeac9229cfb9a360b164a3bf6e3a2d2
suyiiyii <suyiiyii@gmail.com>
2025-01-20 19:47:49 +0800

    成功 rpc 调用

--- app/auth/biz/service/verify_token_by_rpc.go
+++ app/auth/biz/service/verify_token_by_rpc.go
@@ -2,7 +2,11 @@ package service
 
 import (
 	"context"
+	"github.com/cloudwego/kitex/client"
+	consul "github.com/kitex-contrib/registry-consul"
 	auth "github.com/suyiiyii/hertz101/app/auth/kitex_gen/auth"
+	user1 "github.com/suyiiyii/hertz101/rpc_gen/kitex_gen/user"
+	"github.com/suyiiyii/hertz101/rpc_gen/rpc/user"
 )
 
 type VerifyTokenByRPCService struct {
@@ -15,6 +19,18 @@ func NewVerifyTokenByRPCService(ctx context.Context) *VerifyTokenByRPCService {
 // Run create note info
 func (s *VerifyTokenByRPCService) Run(req *auth.VerifyTokenReq) (resp *auth.VerifyResp, err error) {
 	// Finish your business logic.
+	r, err := consul.NewConsulResolver("10.21.22.53:8500")
+	rpcClient, err := user.NewRPCClient("user", client.WithResolver(r))
+	if err != nil {
+		return nil, err
+	}
+	loginResp, err := rpcClient.Login(s.ctx, &user1.LoginReq{
+		Email:    "11",
+		Password: "22",
+	})
+	if err != nil {
+		return nil, err
+	}
 
 	return
 }
