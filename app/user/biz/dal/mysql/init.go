dcb28a84594a1acd72d08cab99abcbc7cede2572
suyiiyii <suyiiyii@gmail.com>
2025-01-21 06:33:01 +0800

    update deps

--- app/user/biz/dal/mysql/init.go
+++ app/user/biz/dal/mysql/init.go
@@ -1,7 +1,6 @@
 package mysql
 
 import (
-	"github.com/joho/godotenv"
 	"github.com/suyiiyii/hertz101/app/user/conf"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -13,10 +12,10 @@ var (
 )
 
 func Init() {
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	panic(err)
+	// }
 	//dsn := fmt.Sprintf(conf.GetConf().MySQL.DSN, os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"), os.Getenv("MYSQL_PORT"))
 	//DB, err = gorm.Open(mysql.Open(dsn),
 	DB, err = gorm.Open(mysql.Open(conf.GetConf().MySQL.DSN),
