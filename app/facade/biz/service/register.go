7973802d968ddcd13b5282e992ce0dd6b39f4046
suyiiyii <suyiiyii@gmail.com>
2025-01-21 06:38:58 +0800

    hertz: add rpc call

--- app/facade/biz/service/register.go
+++ app/facade/biz/service/register.go
@@ -2,9 +2,14 @@ package service
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/kitex/client"
+	consul "github.com/kitex-contrib/registry-consul"
 	facade "github.com/suyiiyii/hertz101/app/facade/hertz_gen/facade"
+	user1 "github.com/suyiiyii/hertz101/rpc_gen/kitex_gen/user"
+	"github.com/suyiiyii/hertz101/rpc_gen/rpc/user"
 )
 
 type RegisterService struct {
@@ -22,5 +27,25 @@ func (h *RegisterService) Run(req *facade.RegisterReq) (resp *facade.RegisterRes
 	// hlog.CtxInfof(h.Context, "resp = %+v", resp)
 	//}()
 	// todo edit your code
+	r, err := consul.NewConsulResolver("10.21.22.53:8500")
+	if err != nil {
+		return nil, err
+	}
+	rpcClient, err := user.NewRPCClient("user", client.WithResolver(r))
+	if err != nil {
+		return nil, err
+	}
+	loginResp, err := rpcClient.Login(h.Context, &user1.LoginReq{
+		Email:    "11",
+		Password: "22",
+	})
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(loginResp)
+
+	resp = &facade.RegisterResp{
+		UserId: loginResp.UserId,
+	}
 	return
 }
